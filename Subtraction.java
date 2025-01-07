1f3490084ac27d695dc37f9af6b43708de29f239
omnema007 <nemaom2003@gmail.com>
2025-01-07 22:28:12 +0530

    Subtraction functionality added

---
+++ Subtraction.java
@@ -0,0 +1,17 @@
+public class Subtraction {
+
+public static int sub(int a, int b) {
+return a - b;
+}
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter first number: ");
+int a = sc.nextInt();
+
+System.out.println("Enter second number: ");
+int b = sc.nextInt();
+
+System.out.println("The result of addition is: ");
+}
+}
