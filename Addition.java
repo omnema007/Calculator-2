faf34e3b179a9244ca6b33451a9e39befe216eb5
omnema007 <nemaom2003@gmail.com>
2025-01-07 16:52:14 +0530

    Addition functinality added

---
+++ Addition.java
@@ -0,0 +1,17 @@
+public class Addition {
+
+public static int add(int a, int b) {
+return a + b;
+}
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter first number: ");
+int a = sc.nextInt();
+
+System.out.println("Enter second number: ");
+int b = sc.nextInt();
+
+System.out.println("The result of addition is: ");
+}
+}
